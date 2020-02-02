c2bf4b769d0ec52e08810f10aa4b3bbcf7ac84ab
zoe <theonlyoneu@daum.net>
2020-02-03 03:36:19 +0900

    add app.ts

---
+++ server/app.ts
@@ -0,0 +1,17 @@
+import * as express from "express";
+
+class App {
+  public app: express.Application;
+
+  constructor() {
+    this.app = express();
+    this.config();
+  }
+
+  private config(): void {
+    this.app.use(express.json());
+    this.app.use(express.static("dist"));
+  }
+}
+
+export default new App().app;
--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,6 @@
-const express = require("express");
-const app = express();
+import app from "./app";
+const PORT = 3000;
 
-app.use(express.json());
-app.use(express.static("dist"));
-
-app.listen(3000, () => console.log("Listening on port 3000!"));
+app.listen(PORT, () => {
+    console.log('Express server listening on port ' + PORT);
+})
